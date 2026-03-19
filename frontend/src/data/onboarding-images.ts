411918d539bf5675b01c57e3801638d4ae79a781
codewithlaurin <loetscher.laurin@stud.hslu.ch>
2026-03-19 21:14:20 +0100

    add onboarding page

---
+++ frontend/src/data/onboarding-images.ts
@@ -0,0 +1,43 @@
+export type OnboardingImageCard = {
+  id: string
+  label: string
+  image: string
+}
+
+export const onboardingImages: OnboardingImageCard[] = [
+    {
+    id: "asian-style",
+    label: "Asian style",
+    image: "/train/Asian.jpg",
+  },
+  {
+    id: "coastal-style",
+    label: "Coastal style",
+    image: "/train/Coastal.jpg",
+  },
+  {
+    id: "contemporary-style",
+    label: "Contemporary style",
+    image: "/train/Contemporary Art.jpg",
+  },
+  {
+    id: "craftsman-style",
+    label: "Craftsman style",
+    image: "/train/Craftsman.jpg",
+  },
+  {
+    id: "eclectic-style",
+    label: "Eclectic style",
+    image: "/train/Eclectic.jpg",
+  },
+  {
+    id: "farmhouse-style",
+    label: "Farmhouse style",
+    image: "/train/Farmhouse.jpg",
+  },
+  {
+    id: "rustic-style",
+    label: "Rustic style",
+    image: "/train/Rustic.jpg",
+  },
+]
