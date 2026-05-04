b5ecab9334ca41c22aa2b6536472109308f4cfa9
Benjamin Amhof <benjamin.amhof@stud.hslu.ch>
2026-05-04 10:45:21 +0200

    hotfix bug due to ruff changes

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
-from app.api import router
 from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parents[2] / ".env")
+
+from app.api import router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-
-load_dotenv(dotenv_path=Path(__file__).resolve().parents[2] / ".env")
 
 app = FastAPI(title="PropertyFinder Backend")
 
